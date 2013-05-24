07c8232a075dd28be90bb7bf2dd8fda2837aa59b
Eduardo Castillejo <eduardo.castillejo@deusto.es>
2013-05-24 17:24:27 +0200

    Refactoring

---
+++ src/com/ecs/android/foursquare/oauth2/OAuth2ClientCredentials.java
@@ -0,0 +1,16 @@
+package com.ecs.android.foursquare.oauth2;
+
+public class OAuth2ClientCredentials {
+
+	public static final String CLIENT_ID = "0AZVAJWROBGEVZEIKSMFDIB0K3LF2Q2HPPHTA0LPYWE3WSHN";
+
+	public static final String CLIENT_SECRET = "JUGJYCLQPSR0A2UGIZH24PXKSBHVR2TUM3H2ED3V3SLDO0IH";
+
+	public static final String AUTHORIZATION_URL = "https://foursquare.com/oauth2/authenticate";
+	
+	public static final String ACCESS_TOKEN_URL = "https://foursquare.com/oauth2/access_token";
+	
+	public static final String REDIRECT_URI = "http://localhost:8888/foursquare/callback";
+
+
+}
